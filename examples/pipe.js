a04f874c79cdd6d0eeb99e251bd728866efcfaba
Alec Brooks <alec.e.brooks@gmail.com>
2018-04-11 16:57:36 +1000

    Non pipe answers

--- examples/pipe.js
+++ examples/pipe.js
@@ -9,7 +9,13 @@ const toPercent = num => num / 100;
 
 const percentStringToNumber = pipe(removePercentSign, toNumber, toPercent);
 
+// Ceci n'est pas une pipe.
+const percentStringToNumber1 = percent => Number(percent.replace('%', '')) / 100;
+const percentStringToNumber2 = percent => toPercent(toNumber(removePercentSign(percent)));
+
 module.exports = {
   pipe,
   percentStringToNumber,
+  percentStringToNumber1,
+  percentStringToNumber2,
 };
